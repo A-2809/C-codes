b465e600533a69e6836d73a3a9dd92272c949534
A-2809 <66512619+A-2809@users.noreply.github.com>
2020-08-04 15:03:19 +0530

    finding the size

---
+++ size.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	char b;
+	float c;
+	double d;
+	cout<< "size of int = ";
+	cout<< sizeof(a);
+	cout<<" bytes\n";
+	cout<< "size of char = ";
+	cout<< sizeof(b);
+	cout<<" bytes\n";
+	cout<< "size of float = ";
+	cout<< sizeof(c);
+	cout<<" bytes\n";
+	cout<< "size of double = ";
+	cout<< sizeof(d);
+	cout<<" bytes\n";
+	return 0;
+}
